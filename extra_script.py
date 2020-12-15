5b905a0ff2e25d66ac2f4a8b32ee2e6559edf5b6
Koxx3 <koxx33@gmail.com>
2020-12-15 01:35:35 +0100

    add hex generation

---
+++ extra_script.py
@@ -0,0 +1,27 @@
+Import("env", "projenv")
+
+# access to global build environment
+print(env)
+
+# access to project build environment (is used source files in "src" folder)
+print(projenv)
+
+# Custom BIN from ELF
+env.AddPostAction(
+	"$BUILD_DIR/${PROGNAME}.elf",
+	env.VerboseAction(" ".join([
+				"$OBJCOPY",
+				"-O",
+				"binary",
+				"$TARGET",
+				"$BUILD_DIR/${PROGNAME}.bin"
+			]), "Building $TARGET"))
+
+# Custom HEX from ELF
+env.AddPostAction(
+    "$BUILD_DIR/${PROGNAME}.elf",
+    env.VerboseAction(" ".join([
+        "$OBJCOPY", "-O", "ihex", "-R", ".eeprom",
+        "$BUILD_DIR/${PROGNAME}.elf", "$BUILD_DIR/${PROGNAME}.hex"
+    ]), "Building $BUILD_DIR/${PROGNAME}.hex")
+)
